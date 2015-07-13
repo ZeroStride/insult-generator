4966d1f6bfad41a6f85b1e43d59be48d4b4f8425
Dayne Broderson <broderson@gmail.com>
2015-07-12 16:50:52 -0800

    fixing require to work as a gem, removing spam

--- lib/insult_generator.rb
+++ lib/insult_generator.rb
@@ -2,13 +2,10 @@ require 'yaml'
 
 class InsultGenerator
   def self.random_insult
-    data = YAML.load_file(File.expand_path("lib/data/insults.yml"))
+    data = YAML.load_file(File.join(File.dirname(__FILE__),"data/insults.yml")))
     column1_rand = data["column1"][rand(50)]
     column2_rand = data["column2"][rand(50)]
     column3_rand = data["column3"][rand(50)]
     "Thou #{column1_rand} #{column2_rand} #{column3_rand} build!"
   end
 end
-
-#Throw an insult when required.
-p InsultGenerator.random_insult
