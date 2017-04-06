4db40e0514b7376672820d61ad4b67f84299805b
Pat Wilson <zerostride@gmail.com>
2017-04-06 12:30:28 -0700

    Fix data file path

--- lib/insult_generator.rb
+++ lib/insult_generator.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 class InsultGenerator
   def self.random_insult
-    data = YAML.load_file(File.expand_path("lib/data/insults.yml"))
+    data = YAML.load_file(File.expand_path("data/insults.yml", File.dirname(__FILE__)))
     column1_rand = data["column1"][rand(50)]
     column2_rand = data["column2"][rand(50)]
     column3_rand = data["column3"][rand(50)]
